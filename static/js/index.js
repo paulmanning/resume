a55a48efcd6f852e5f8fd603dcbb78e2b0f19220
Paul Manning <mortman1@gmail.com>
2016-09-07 14:25:41 -0700

    Adding underscore and jQuery with bower.

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,37 @@
+/*jslint browser: true*/
+
+var config = {
+    resumePath: '../data/resume.json', 
+    rootElement: 'app',
+    dateFormat: "MMM, YYYY"
+}; 
+
+
+(function(config, $, _){
+    //load json
+    var path = config.resumePath;    
+
+    //load the JSON file and callback with string 
+    //  representation
+    function loadJSON(path, callback) {
+        var xhr = new XMLHttpRequest();
+            xhr.overrideMimeType("application/json");
+        xhr.open('GET', path, true);
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState == 4 && xhr.status == 200) {
+                callback(xhr.responseText);
+            }
+        };
+        xhr.send(null);
+    }
+    
+    loadJSON(path, function(val){
+        var obj = JSON.parse(val);
+        document.getElementById(config.rootElement)
+            .innerHTML = obj.basics.name.toString();
+        //alert(val);
+        _.each(["hi"], alert);
+    });
+    
+})(config, window.$, window._);
 
